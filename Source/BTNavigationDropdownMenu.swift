5137c1c3581ad8af93ca5c04143eb51d5b15ffca
Piny <zuopeng@ygsoft.com>
2020-11-05 18:40:41 +0800

    no message

--- Source/BTNavigationDropdownMenu.swift
+++ Source/BTNavigationDropdownMenu.swift
@@ -384,7 +384,7 @@ open class BTNavigationDropdownMenu: UIView {
 
     override open func layoutSubviews() {
         self.menuTitle.sizeToFit()
-        self.menuTitle.center.x = self.menuButton.size.width/2
+        self.menuTitle.center.x = self.menuButton.frame.size.width/2
         self.menuTitle.center.y = self.frame.size.height/2
 //        self.menuTitle.center = CGPoint(x:  self.menuButton.size.width/2, y:  self.menuButton.size.height/2)
         self.menuTitle.textColor = self.configuration.menuTitleColor
